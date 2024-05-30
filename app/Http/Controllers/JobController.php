7de21276dea980104f96ed9971e416b47e25c8d1
Rakesh <rakeshbhandari@gmail.com>
2024-05-30 14:16:37 +0545

    resource controller

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //get all jobs with their employers and order them by the latest created with pagination
+        $jobs = Job::with('employer')->latest()->paginate(10);
+
+        //return the view with the jobs data 
+        return view(
+            'jobs.index',
+            ['jobs' => $jobs]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validate
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'description' => 'required',
+            'salary' => ['required', 'numeric'],
+        ]);
+
+        //store
+        Job::create([
+            'title' => request('title'),
+            'description' => request('description'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+        //redirect
+        return redirect('/jobs');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Job $job)
+    {
+        //find the job by id
+        // NOTE: we can use the $job variable directly because of the route model binding
+        // $job = Job::find($id); 
+        //return the view with the job
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Job $job)
+    {
+        //find the job
+        // NOTE: we can use the $job variable directly because of the route model binding
+        // $job = Job::find($id);
+        //return the create view with the job
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Job $job)
+    {
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'description' => 'required',
+            'salary' => ['required', 'numeric'],
+        ]);
+
+        // $job = Job::findOrFail($id);
+
+        $job->update([
+            'title' => request('title'),
+            'description' => request('description'),
+            'salary' => request('salary'),
+        ]);
+        return redirect('/jobs/' . $job->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Job $job)
+    {
+        //delete the job
+        // $job = Job::findOrFail($id);
+        $job->delete();
+
+        //redirect 
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,108 +1,32 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 //home page
-Route::get('/', function () {
+// Route::get('/', function () {
 
-    return view('home');
-});
+//     return view('home');
+// });
+// Route::view does the same thing as the above code 
+// only applicable for static pages
+Route::view('/', 'home');
 
 //contact page
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
 
 
-//index : all jobs 
-Route::get('/jobs', function () {
-    //get all jobs with their employers and order them by the latest created with pagination
-    $jobs = Job::with('employer')->latest()->paginate(10);
-
-    //return the view with the jobs
-    return view(
-        'jobs.index',
-        ['jobs' => $jobs]
-    );
-});
-
-//create new job
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-
-//store new job
-Route::post('/jobs', function (Request $request) {
-    //validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'description' => 'required',
-        'salary' => ['required', 'numeric'],
-    ]);
-
-    //store
-    Job::create([
-        'title' => request('title'),
-        'description' => request('description'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-    //redirect
-    return redirect('/jobs');
-});
-
-//show job
-Route::get('/jobs/{id}', function ($id) {
-    //find the job
-    $job = Job::find($id);
-    //return the view with the job
-    return view('jobs.show', ['job' => $job]);
-});
-
-
-//edit job
-Route::get('/jobs/{id}/edit', function ($id) {
-    //find the job
-    $job = Job::find($id);
-    //return the create view with the job
-    return view('jobs.edit', ['job' => $job]);
-});
-
-
-//update job
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'description' => 'required',
-        'salary' => ['required', 'numeric'],
-    ]);
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'description' => request('description'),
-        'salary' => request('salary'),
-        
-
-    ]);
-
-    
-    return redirect('/jobs/' . $job->id);
-});
-
-
-//delete job
-Route::delete('/jobs/{id}', function ($id) {
-
-
-    //delete the job
-    $job = Job::findOrFail($id);
-    $job->delete();
-
-    //redirect 
-    return redirect('/jobs');
-});
+//NOTE: JobController routes are grouped together
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+// NOTE: route resource for JobController which does the same thing as the above code
+Route::resource('jobs', JobController::class);
